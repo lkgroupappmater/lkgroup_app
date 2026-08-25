ffb94442252fb563ea5cf06c38e0e7e9a58f2f95
lkgroupappmater <lkgroup112@gmail.com>
2026-08-25 12:26:56 +0700

    Initial Commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,15 @@
-// lib/main.dart
-
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+
 import 'app.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Supabase.initialize(
+    url: 'https://rkqwzxfcnciptnwesfbr.supabase.co',
+    publishableKey: 'sb_publishable_O1kCnQpkg9SMDVcBSrOW5g_qzASvJCJ',
+  );
+
   runApp(const CargoFlowApp());
 }
